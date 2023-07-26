2d732863c38af4afce0f28be7f3894df18398c73
Alexander Ma <arma1@uci.edu>
2023-07-25 21:25:01 -0500

    Start command setting, set up home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,16 @@
 import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [text, setText] = useState<string>('Press me');
-
-  const handleClick = () => {
-    fetch('http://localhost:8000/health-check')
-      .then((response: Response) => {
-        return response.text();
-      })
-      .then((message: string) => {
-        setText(message);
-      });
-  };
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-        <button onClick={handleClick}>{text}</button>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
