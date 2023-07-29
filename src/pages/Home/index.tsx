19081fdf5d1f8e925bac2dd61a365f5717fd21c9
Alexander Ma <arma1@uci.edu>
2023-07-28 20:34:02 -0500

    Added config for eslinter

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-
+import React, { SetStateAction } from 'react';
+import image from '../../assets/images/master.png';
 function Home() {
   return (
     <div>
       <h1>Hello</h1>
+      <img src={image}></img>
     </div>
   );
 }
