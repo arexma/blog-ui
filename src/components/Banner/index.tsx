d2ff2efc0a637d4153e7487ca782d1542890e198
Alexander Ma <arma1@uci.edu>
2023-08-16 05:16:00 -0500

    Made banner automatically scroll every 5 seconds and from whatever position it's currently on; also resets timer whenever its position is changed

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,22 +1,47 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import styles from './Banner.module.scss';
-import TFT from '../../assets/images/master.png';
-import Hololive from '../../assets/images/hololive_en_non_fiction.svg';
-import Loi from '../../assets/images/loi_pfp.jpg';
-
+import Blog1 from '../../assets/images/master.png';
+import Blog2 from '../../assets/images/hololive_en_non_fiction.svg';
+import Blog3 from '../../assets/images/loi_pfp.jpg';
+const Blog4 = '';
 export const Banner = () => {
   // Banners will be path names to whatever blog posts we're currently showing
-  const banners: { [path: string]: string } = { '/': TFT, '/1': Hololive, '/2': Loi, '/3': '' };
   // Blog path --> img path
+  const banners: { [path: string]: string } = { '/': Blog1, '/1': Blog2, '/2': Blog3, '/3': Blog4 };
+
   const [clicked, setClicked] = useState<string>('/');
+  const keys = Object.keys(banners);
+  let intervalID: NodeJS.Timeout | undefined = undefined;
+
+  const startInterval = () => {
+    intervalID = setInterval(() => {
+      setClicked(clicked => keys[(keys.findIndex(element => element === clicked) + 1) % keys.length]);
+    }, 5000);
+  };
+
+  const handleClicked = (path: string) => {
+    clearInterval(intervalID);
+    setClicked(path);
+    startInterval();
+  };
+
+  useEffect(() => {
+    startInterval();
+    return () => {
+      if (intervalID) {
+        clearInterval(intervalID);
+      }
+    };
+  }, [clicked]);
 
   return (
     <div className={styles.Banner}>
       <input type="image" src={banners[clicked]}></input>
       <div>
         {Object.keys(banners).map((path: string) => (
-          <input type="radio" key={path} onChange={() => setClicked(path)} checked={path === clicked} />
+          <input type="radio" key={path} onChange={() => handleClicked(path)} checked={path === clicked} />
         ))}
+        {/* Maybe memo input tag for performance optimization */}
       </div>
     </div>
   );
