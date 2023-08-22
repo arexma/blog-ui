ef016c04d6d7da459ec03884b2d40c0f4b6364c1
Alexander Ma <maalexander2@yahoo.com>
2023-08-22 04:29:37 -0700

    Fixed up HomeBottomUI to use margin instead of padding, inverted icon colors

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -9,26 +9,28 @@ export const Footer = () => {
       <div className={styles.AboutUs}>
         <h6>About Us</h6>
       </div>
-      <div className={styles.Alexander}>
-        <h6>Alexander Ma:</h6>
-        <a href={'https://www.linkedin.com/in/alexander-ma-2956a2216/'} target={'_blank'}>
-          <img src={LinkedIn}></img>
-        </a>
-        <a href={'https://github.com/arexma'} target={'_blank'}>
-          <img src={Github}></img>
-        </a>
-      </div>
-      <div className={styles.Loi}>
-        <h6>Loi Nguyen:</h6>
-        <a href={'https://www.linkedin.com/in/loi-nguyen-9aaa14252/'} target={'_blank'}>
-          <img src={LinkedIn}></img>
-        </a>
-        <a href={'https://github.com/LoiVNguyen'} target={'_blank'}>
-          <img src={Github}></img>
-        </a>
-      </div>
-      <div className={styles.Extra}>
-        <h6>Privacy Policy</h6>
+      <div className={styles.Row}>
+        <div className={styles.Alexander}>
+          <h6>Alexander Ma:</h6>
+          <a href={'https://www.linkedin.com/in/alexander-ma-2956a2216/'} target={'_blank'}>
+            <img src={LinkedIn}></img>
+          </a>
+          <a href={'https://github.com/arexma'} target={'_blank'}>
+            <img src={Github}></img>
+          </a>
+        </div>
+        <div className={styles.Loi}>
+          <h6>Loi Nguyen:</h6>
+          <a href={'https://www.linkedin.com/in/loi-nguyen-9aaa14252/'} target={'_blank'}>
+            <img src={LinkedIn}></img>
+          </a>
+          <a href={'https://github.com/LoiVNguyen'} target={'_blank'}>
+            <img src={Github}></img>
+          </a>
+        </div>
+        <div className={styles.Extra}>
+          <h6>Privacy Policy</h6>
+        </div>
       </div>
     </div>
   );
